10c51924a1ea295d5124bd9e519eff734b78dcfc
7b809 <111459449+7b809@users.noreply.github.com>
2024-05-04 19:00:11 +0530

    Update get_pdf.py

--- get_pdf.py
+++ get_pdf.py
@@ -2,28 +2,25 @@
 from datetime import datetime
 from pymongo import MongoClient
 import json
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.dates import DateFormatter
 from matplotlib.backends.backend_pdf import PdfPages
 import io
+import os
 
 mongodb_uri = os.environ.get('MONGODB_URI')
 
-
-# MongoDB URI
-
-# Print database connection status
+# Connect to MongoDB
 print("Connecting to MongoDB...")
 client = MongoClient(mongodb_uri)
 print("Connected to MongoDB successfully!")
 
 # Get the database
-db = client.mydatabase  # You can replace 'mydatabase' with your desired database name
+db = client.mydatabase
 
-# Get the collection
+# Retrieve data from MongoDB collection
 collection_name = 'asinc_profits'
 print("Retrieving data from collection '{}'...".format(collection_name))
 collection_data = list(db[collection_name].find())
@@ -32,38 +29,55 @@ collection_data = list(db[collection_name].find())
 for document in collection_data:
     document['_id'] = str(document['_id'])
 
-# Parse JSON string to Python object
+# Convert JSON string to Python object
 data = json.dumps(collection_data, indent=4)
 data = json.loads(data)
 
+# Get the current year
 current_year = datetime.now().year
+
 # Preprocess data
 for item in data:
     # Modify profit column
     item['profit'] = item['rentability']
     profit_str = item['profit']
-    # Extracting only the numerical value
     try:
         profit_num = float(profit_str.split('$')[1].split('/')[0])
-    except IndexError:
-        # Handle cases where the format doesn't match expected pattern
+    except (IndexError, ValueError):
         profit_num = None
     item['profit'] = profit_num
+    
+    power_consumption_str = item['power_consumption']
+    try:
+        power_consumption_num = float(power_consumption_str.split('W')[0])  # Extracting watts
+        # Convert power consumption from watts to kilowatts and then calculate electricity cost for a day
+        electricity_cost = (power_consumption_num / 1000) * 24 * 0.12  # Assuming electricity cost of $0.12 per kWh
+    except (IndexError, ValueError):
+        # Handle cases where the format doesn't match expected pattern
+        electricity_cost = None
+
+    # Ensure electricity_cost is a float, setting it to 0 if it's None
+    if electricity_cost is None:
+        electricity_cost = 0.0
+
+    # Ensure profit is a float, setting it to 0 if it's None
+    if item['profit'] is None:
+        item['profit'] = 0.0
+
+    item['full_profit'] = item['profit'] + electricity_cost
 
     # Convert timestamp to datetime object
     timestamp_str = item['updated_timestamp']
     timestamp_datetime = datetime.strptime(timestamp_str, "%A, %b %d, %Y, %I %p")
-    # Check if timestamp contains year, if not, replace with current year
     if timestamp_datetime.year == 1900:
         timestamp_datetime = timestamp_datetime.replace(year=current_year)
-    # Format timestamp to include only the hour part
     timestamp_hour = timestamp_datetime.strftime('%Y-%m-%d %H')
     item['timestamp'] = timestamp_hour
 
 # Filter out entries with None profit values
 data = [item for item in data if item['profit'] is not None]
 
-# Create a dictionary to store data grouped by machine name
+# Group data by machine name
 machine_data_dict = {}
 for item in data:
     name = item['name']
@@ -100,16 +114,15 @@ try:
 except ValueError:
     cmap = cm.tab10
 
-
 # Sort the combined data by profit in descending order
 sorted_combined_data = {k: v for k, v in sorted(combined_data.items(), key=lambda item: max(item[1], key=lambda x: x['profit'])['profit'], reverse=True)}
 
 # Create a PDF object to save plots
-with PdfPages("all_plots_sorted.pdf") as pdf:
+with PdfPages("testing_one.pdf") as pdf:
     # Iterate over each sheet in sorted_combined_data
     for idx, (sheet_name, sheet_data) in enumerate(sorted_combined_data.items(), start=1):
         # Create a new figure for each sheet
-        plt.figure(figsize=(24, 11))
+        fig, axs = plt.subplots(1, 2, figsize=(36, 12))
 
         # Convert sheet data to DataFrame
         df = pd.DataFrame(sheet_data)
@@ -123,39 +136,44 @@ with PdfPages("all_plots_sorted.pdf") as pdf:
         # Plot scatter plot
         # Iterate over each hour category
         for category, group in df.groupby('hour_category'):
+            axs[0].scatter(group['timestamp'], group['profit'], label=category, color=cmap(group['timestamp'].dt.hour.iloc[0] // 6))
+            axs[0].set_title('Original Profit (with Electricity included 0.12$ )')
+            axs[0].set_xlabel('Timestamp')
+            axs[0].set_ylabel('Profit ($)')
+            axs[0].legend()
             
-            plt.scatter(group['timestamp'], group['profit'], label=category, color=cmap(group['timestamp'].dt.hour.iloc[0] // 6), alpha=0.5)
+            axs[1].scatter(group['timestamp'], group['full_profit'], label=category, color=cmap(group['timestamp'].dt.hour.iloc[0] // 6))
+            axs[1].set_title('Full Profit (with out Electricity included 0.12$ )')
+            axs[1].set_xlabel('Timestamp')
+            axs[1].set_ylabel('Profit ($)')
+            axs[1].legend()
 
-            
+            # Apply settings for both scatter plots
+            for ax in axs:
+                ax.set_xticks(ax.get_xticks())  # Force applying rotation to x-axis ticks
+                ax.xaxis.set_major_locator(plt.MaxNLocator(6))  # Display only 6 ticks
+                ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))  # Format the tick labels as desired
+            axs[0].grid(True)
+            axs[1].grid(True)
 
-
-        plt.title(f"{df['name'][0]} {sheet_name} Profits")
-        plt.xlabel('Timestamp')
-        plt.ylabel('Profit ( $ )')
-        plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
-
-        # Customize x-axis ticks to display only specific dates or intervals
-        plt.gca().get_xaxis().set_major_locator(plt.MaxNLocator(6))  # Display only 6 ticks
-        plt.gca().get_xaxis().set_major_formatter(DateFormatter('%Y-%m-%d'))  # Format the tick labels as desired
-
+        plt.suptitle(f"{df['name'][0]} {sheet_name} Profits")
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust subplot layout to accommodate suptitle
         plt.legend(title='Hour Category', bbox_to_anchor=(1.05, 1), loc='upper left')
-        plt.tight_layout()
 
         # Save the current figure to the PDF
         pdf.savefig()
         plt.close()
 
         # Print completion message for every 10 images
-        if idx % 10 == 0:
+        if idx % 50 == 0:
             print(f"{idx} out of {len(sorted_combined_data)} sheets processed.")
 
     # Print completion message
     print("Plots for all sheets sorted by profits merged into PDF.")
-# After saving the PDF, proceed to save it into MongoDB
-# Define the filename for the PDF
-pdf_filename = "all_plots_sorted.pdf"
 
-collection = db.pdf_data  # Define the collection to store PDF data
+# Save the PDF to MongoDB collection
+pdf_filename = "testing_one.pdf"
+collection = db.pdf_data
 
 # Check if a document with the same filename exists
 existing_pdf = collection.find_one({"filename": pdf_filename})
