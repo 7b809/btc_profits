c545b23cae7b5542c8c9d474ee322430c852b440
7b809 <111459449+7b809@users.noreply.github.com>
2024-05-04 22:10:24 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,31 +1,25 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, request, send_file
 from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
+@app.route('/get_pdf', methods=['GET'])
+def get_pdf():
+    filename = request.args.get('filename')
+    if not filename:
+        return 'Error: Missing filename parameter', 400
+    
+    # Connect to MongoDB Atlas
+    mongodb_uri = os.environ.get('MONGODB_URI')
+    client = MongoClient(mongodb_uri)
+    db = client.mydatabase  # Replace 'mydatabase' with your database name
+    collection = db.pdf_data
 
-# Connect to MongoDB Atlas
-
-mongodb_uri = os.environ.get('MONGODB_URI')
-client = MongoClient(mongodb_uri)
-db = client.mydatabase  # Replace 'mydatabase' with your database name
-collection = db.pdf_data
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/download-pdf')
-def download_pdf():
-    query = {"filename": "testing_one.pdf"}
+    # Download the PDF File from MongoDB Atlas
+    query = {"filename": filename}
     pdf_document = collection.find_one(query)
     if pdf_document:
         pdf_data = pdf_document["data"]
-        with open("downloaded_pdf.pdf", "wb") as f:
-            f.write(pdf_data)
-        return send_file("downloaded_pdf.pdf", as_attachment=True)
+        return send_file(pdf_data, as_attachment=True, attachment_filename=f'{filename}.pdf')
     else:
-        return "PDF document not found in MongoDB Atlas."
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return 'Error: PDF document not found', 404
